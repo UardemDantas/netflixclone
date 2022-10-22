0a688da0f39149e77bc1932f6e4acc9d037a145b
Uardem Dantas <uardemj12@gmail.com>
2022-10-22 13:46:18 -0300

    inital commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,16 +11,16 @@ function Navbar() {
 
   return (
     <div className={`nav-container ${show && "nav-container-black"}`}>
-      <img
+      <a href="#"> <img
         className="nav-logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix"
-      />
-      <img
+      /></a>
+      <a href="#"><img
         className="nav-avatar"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPsVAeFlYeYOEUzb3TV1ML91_LPkkFML5lRQcMdr9nQu2CqO-WzT-RLmkM5_cOKvkaBkI&usqp=CAU"
         alt="Uardem"
-      />
+      /></a>
     </div>
   );
 }
